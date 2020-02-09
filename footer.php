2784fdfb7f128e4669a480cbb2f8c79ad39c4157
beuspectre <35420058+beuspectre@users.noreply.github.com>
2020-02-09 15:26:29 +0100

    Front
    front templates

--- footer.php
+++ footer.php
@@ -1,16 +1,24 @@
-</div>
-    <!-- /#wrapper -->
+ <div class="container">
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Innovation Tech 2020</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="js/plugins/morris/raphael.min.js"></script>
-    <script src="js/plugins/morris/morris.min.js"></script>
-    <script src="js/plugins/morris/morris-data.js"></script>
 
 </body>
 
--- top_nav.php
+++ top_nav.php
@@ -1,22 +1,59 @@
-<div class="navbar-header">
-<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-    <span class="sr-only">Toggle navigation</span>
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-</button>
-<a class="navbar-brand" href="index.php"> Admin Superuser</a>
-</div>
- <!-- Top Menu Items -->
-            <ul class="nav navbar-right top-nav">
-              <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Admin <b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                       
-                        <li class="divider"></li>
-                        <li>
-                            <a href="logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
+
+<th></th><th></th><th></th>
+<div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+        
+
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <img class= "logo" src="/ecom/img/logo.png" style="height:60px; padding-left:2px;">
+                <a class="navbar-brand" href="index.php">Innovation Tech </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="shop.php">Shop</a>
+                    </li>
+                    
+                                        <li>
+                        <a href="checkout.php">Checkout</a>
+                    </li>
+                    <li>
+                        <a href="contact.php">Contact</a>
+                    </li>
+
+                    <?php
+                    if(isset($_SESSION['logged']))
+                    {
+                        echo "<li><a href=''>".$_SESSION['username']."</a></li>";
+                        echo "<li><a href='logout.php'>LOG OUT</a></li>";
+
+                  
+                    }
+                    else{
+                          echo" <li>
+                        <a href='logIn1.php'> Login</a>
+                    </li>";
+                    echo"<li>
+                        <a href='regjitrohu1.php'> Register Now</a>
+                    </li>";
+                     echo"<li>
+                        <a href='login.php'>Login ADMIN</a>
+                    </li>";
+                     echo"<li>
+                        <a href='admin'><strong>We are the right choice for your technology solutions</strong></a>
+                    </li>";
+
+               
+                    } 
+                    ?> </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
